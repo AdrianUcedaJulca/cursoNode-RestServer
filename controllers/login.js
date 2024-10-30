c9a276db7983a8edb6ee84086176979777bc5bf1
Adrian Uceda Julca <adrianucedajulca@gmail.com>
2024-10-30 11:42:00 -0500

    Proyecto finalizado

--- controllers/login.js
+++ controllers/login.js
@@ -2,7 +2,10 @@ const { response } = require("express");
 const bcryptjs = require("bcryptjs");
 
 const Usuario = require("../models/usuario");
+
 const { generarJwt } = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
+const usuario = require("../models/usuario");
 
 
 const login = async(req, res = response) => {
@@ -50,6 +53,55 @@ const login = async(req, res = response) => {
     }
 }
 
+const googleSignIn = async(req, res = response) => {
+
+    const { id_token } = req.body;
+
+    try {
+
+        const { correo, nombre, img } = await googleVerify( id_token );
+
+        let usuario = await Usuario.findOne({ correo });
+
+        //Crar usuario si es que no existe en la base de datos ya que se esta logeando con google
+        if( !usuario ){
+
+            const data = {
+                nombre,
+                correo,
+                img,
+                google: true,
+                password: ':P',
+                rol: 'USER_ROL'
+            };
+
+            usuario = new Usuario(data);
+            await usuario.save();
+            console.log(usuario);
+        }
+
+        //Verificar que el usuario este activo
+        if( !usuario.estado ){
+
+            return res.status(401).json({
+                msg: 'Hable con el adminitrador - usuario inactivo'
+            });
+        }
+
+        //Generear jwt
+        const token = await generarJwt( usuario.id );
+
+        res.json({
+            usuario,
+            token
+        });
+    } catch (error) {
+        
+        
+    }
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
